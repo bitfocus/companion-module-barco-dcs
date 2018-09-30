468a087b4b5da1b6833f6cb21a74891525585c04
William Viker <william.viker@gmail.com>
2018-10-01 01:42:47 +0200

    module update from core change: change the way we store versioning and module info to package.json

--- index.js
+++ index.js
@@ -359,11 +359,5 @@ instance.prototype.action = function(action) {
 	}
 };
 
-instance.module_info = {
-	label: 'Barco dcs',
-	id: 'dcs',
-	version: '0.0.1'
-};
-
 instance_skel.extendedBy(instance);
 exports = module.exports = instance;
