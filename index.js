6f3a3f0b6b552d8cf2a4b3ff5817e502f66fbcb9
Julian Waller <git@julusian.co.uk>
2022-02-02 16:42:19 +0000

    bulk replace system.emit calls

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ instance.prototype.destroy = function() {
 
 instance.prototype.actions = function(system) {
 	var self = this;
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 
 		'command': {
 			label:'Run Command',
